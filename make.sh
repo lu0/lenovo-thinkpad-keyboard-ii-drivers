b97330c66f008fc3544659e3db38e7802edf33d4
lu0 <me@lucerocodes.com>
2022-05-21 23:47:46 -0500

    Format make file

--- make.sh
+++ make.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-for i in /lib/modules/*/build; do KDIR="$i" make clean; KDIR="$i" make; sudo KDIR="$i" make modules_install; done
+for i in /lib/modules/*/build; do
+    KDIR="$i" make clean
+    KDIR="$i" make
+    sudo KDIR="$i" make modules_install
+done
